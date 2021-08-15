a4774c6463fa049de0f6d2d53fceb6fb77aaf1a8
CamiloRobayo <chosey117@gmail.com>
2021-08-15 17:24:08 -0500

    terminacion crod de clientes

---
+++ evidencia1/Controllers/ClientesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using evidencia1.Models;
+namespace evidencia1.Controllers
+{
+    public class ClientesController : Controller
+    {
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            using (var db = new inventario_2021Entities())
+            {
+                return View(db.cliente.ToList());
+            }
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult Create(cliente cliente)
+        {
+            if (!ModelState.IsValid)
+                return View();
+
+            try
+            {
+                using (var db = new inventario_2021Entities())
+                {
+                    db.cliente.Add(cliente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+
+
+            }
+
+        }
+        public ActionResult Details(int id)
+        {
+            using (var db = new inventario_2021Entities())
+            {
+                var findente = db.cliente.Find(id);
+                return View(findente);
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                using (var db = new inventario_2021Entities())
+                {
+                    var findente = db.cliente.Find(id);
+                    db.cliente.Remove(findente);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        public ActionResult Edit(int id)
+        {
+            try
+            {
+                using (var db = new inventario_2021Entities())
+                {
+                    cliente findente = db.cliente.Where(a => a.id == id).FirstOrDefault();
+                    return View(findente);
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(cliente editente)
+        {
+            try
+            {
+
+                using (var db = new inventario_2021Entities())
+                {
+                    cliente ente = db.cliente.Find(editente.id);
+
+                    ente.nombre = editente.nombre;
+                    ente.documento = editente.documento;
+                    ente.email = editente.email;
+
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+
+
+        }
+    }
+
+}
+        
+    
