18c4b3ea4ac408e29112a72b2a190a49506b6106
CamiloRobayo <chosey117@gmail.com>
2021-09-08 16:36:10 -0600

    Terminacion login

--- evidencia1/Controllers/UsuariosController.cs
+++ evidencia1/Controllers/UsuariosController.cs
@@ -4,18 +4,25 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using evidencia1.Models;
+using System.Web.Security;
+using System.Text;
 namespace evidencia1.Controllers
 {
     public class UsuariosController : Controller
     {
-        // GET: Usuarios
+        [Authorize]
+        // GET: Usuario
         public ActionResult Index()
         {
+
+            //conexion a la base de datos
             using (var db = new inventario_2021Entities2())
             {
                 return View(db.usuario.ToList());
             }
         }
+
+
 
         public ActionResult Create()
         {
@@ -32,6 +39,7 @@ namespace evidencia1.Controllers
             {
                 using (var db = new inventario_2021Entities2())
                 {
+                    usuario.password = UsuariosController.HashSHA1(usuario.password);
                     db.usuario.Add(usuario);
                     db.SaveChanges();
                     return RedirectToAction("Index");
@@ -44,6 +52,19 @@ namespace evidencia1.Controllers
 
 
             }
+        }
+        public static string HashSHA1(string value)
+        {
+            var sha1 = System.Security.Cryptography.SHA1.Create();
+            var inputBytes = Encoding.ASCII.GetBytes(value);
+            var hash = sha1.ComputeHash(inputBytes);
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
         }
 
         public ActionResult Details(int id)
@@ -69,7 +90,7 @@ namespace evidencia1.Controllers
             catch (Exception ex)
             {
                 ModelState.AddModelError("", "error " + ex);
-                return View();
+                return RedirectToAction("Index");
             }
         }
 
@@ -121,6 +142,47 @@ namespace evidencia1.Controllers
 
 
         }
+
+        public ActionResult Login(string mensaje = "")
+        {
+            ViewBag.Message = mensaje;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string user, string password)
+        {
+            try
+            {
+                string passEncrip = UsuariosController.HashSHA1(password);
+                using (var db = new inventario_2021Entities2())
+                {
+                    var userLogin = db.usuario.FirstOrDefault(e => e.email == user && e.password == passEncrip);
+                    if (userLogin != null)
+                    {
+                        FormsAuthentication.SetAuthCookie(userLogin.email, true);
+                        Session["User"] = userLogin;
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        return Login("Verifique sus datos");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", "error " + ex);
+                return View();
+            }
+        }
+
+        [Authorize]
+        public ActionResult CloseSession()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
     }
-    
 }
---
+++ evidencia1/Models/Reporte.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace evidencia1.Models
+{
+    public class Reporte
+    {
+
+        public String NombreProveedor { get; set; }
+        public String Direeccion { get; set; }
+        public String Telefono { get; set; }
+        public String nombreProducto { get; set; }
+        public int PrecioProducto { get; set; }
+
+    }
+}
